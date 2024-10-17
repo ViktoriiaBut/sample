43c7969caf247d31348b43b6b1122e1340877517
ViktoriiaBut <zironka.tory@gmail.com>
2024-10-17 21:16:44 +0100

    sam

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+const a = 30;
+const b = 90;
 
-function foo(username) {
-    console.log(`Hello ${username}`);
-    return "Zhj0ckfdyf"
-}
-
-const user = foo("Ali");
-
-console.log("user", user);
+for(let i = b; i >= a; i--) {
+    if(!(i % 5)) {
+        console.log(i);
+    }
+   
+   }
