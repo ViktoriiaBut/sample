6861d9cad219c3e8064fcaef3d3e5f5553571c84
ViktoriiaBut <zironka.tory@gmail.com>
2024-10-22 19:59:41 +0100

    zz

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function slugify(title) {
-    
-    let arr = title.split(" ");
-    return(arr.join("-").tolowerCase); 
-
+function filterArray(numbers, value) {
+ let array = [];
+ for (let i = 0; i < numbers.length; i++) {
+    if (value < numbers[i]) {
+        array.push(numbers[i]);
+    }
+ }
+ return array;
 }
 
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
